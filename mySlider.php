30d2bc08a5e562e206f694462925fa9085baada7
NesarAhmedRazon <grim.roy@gmail.com>
2022-01-19 00:03:56 +0600

    update1

--- mySlider.php
+++ mySlider.php
@@ -15,18 +15,54 @@ function make_shortcode(){
     wp_enqueue_script('slider-lib', plugin_dir_url( __FILE__ ).'assets/slick/slick.js', array(), null, true);
     wp_enqueue_script('slider-app', plugin_dir_url( __FILE__ ).'assets/app.js', array(), null, true);
 
-
-    ob_start(); ?>
-
-        <div class="sliderContainer">
+if(is_page()){
+    ob_start(); 
+    $content = the_content();
+    
+    global $post;
+ 
+    $myposts = get_posts( array(
+        'post_type'  => 'project',
+        'posts_per_page' => 5,
+        'offset'         => 1,
+        'category'       => 1
+    ) );
+ 
+    if ( $myposts ) {
+        foreach ( $myposts as $post ) : 
+            setup_postdata( $post ); ?>
+            <div class="sliderContainer">
+            <?php if ( has_post_thumbnail() ) : ?>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                </a>
+            <?php endif; ?>
                 <img src="<?php echo plugin_dir_url( __FILE__ );?>/assets/preview3.jpg">
                 <img src="<?php echo plugin_dir_url( __FILE__ );?>/assets/preview2.jpg">
                 <div class="swiper-slide">Slide 3</div>
-            </div>  
+        </div>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        <?php
+        endforeach;
+        wp_reset_postdata();
+    }
+    ?>
+
         
+    
     <?php $projectData = ob_get_clean();
-return $projectData;
+    return $projectData;
+}
+
     
 }
 
 add_shortcode( 'myslider', 'make_shortcode' );
+
+
+function theTitleShortCode(){
+    $title = get_the_title();
+    return $title;
+}
+
+add_shortcode( 'mytitle', 'theTitleShortCode' );
