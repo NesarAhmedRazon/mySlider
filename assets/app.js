7a375f782cb6ee4cdb04453b1a312ce9aad17620
NesarAhmedRazon <grim.roy@gmail.com>
2022-01-20 15:22:56 +0600

    Update 3

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,53 @@
 jQuery.noConflict();
 (function ($) {
   $(function () {
-    $(".sliderContainer").outerHeight(
-      (9 / 16) * $(".sliderContainer").outerWidth()
-    );
-    $(window).on("resize", function () {
-      $(".sliderContainer").outerHeight(
-        (9 / 16) * $(".sliderContainer").outerWidth()
-      );
+    $(window).on("load", function () {
+      $(".postcard").first().find(".postcard_body").addClass("show");
     });
-    $(".sliderContainer").slick({
-      autoplay: true,
-      autoplaySpeed: 2000,
+    $(".postcard .sliderContainer .slider").on("click", function () {
+      console.log("clicked");
+      $(this)
+        .closest(".postcard")
+        .siblings()
+        .find(".postcard_body")
+        .removeClass("show");
+      $(this).closest(".postcard").find(".postcard_body").toggleClass("show");
+    });
+
+    //Slider Init
+    $(".slider").outerHeight((9 / 16) * $(".slider").outerWidth());
+    $(window).on("resize", function () {
+      $(".slider").outerHeight((9 / 16) * $(".slider").outerWidth());
+    });
+
+    $(".sliderContainer .prev").on("click", function () {
+      $(this).closest(".postcard").find(".slider").slick("slickPrev");
+    });
+    $(".sliderContainer .next").on("click", function () {
+      $(this).closest(".postcard").find(".slider").slick("slickNext");
+    });
+
+    $(".sliderNavcontainer .prev").on("click", function () {
+      $(this).closest(".postcard").find(".slider-nav").slick("slickPrev");
+    });
+    $(".sliderNavcontainer .next").on("click", function () {
+      $(this).closest(".postcard").find(".slider-nav").slick("slickNext");
+    });
+
+    $(".slider").slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
       fade: true,
       cssEase: "linear",
+      asNavFor: ".slider-nav",
+    });
+    $(".slider-nav").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: false,
+      centerMode: true,
+      focusOnSelect: true,
     });
   });
 })(jQuery);
--- mySlider.php
+++ mySlider.php
@@ -9,6 +9,10 @@ Version: 0.0.1
 Author URI: https://github.com/NesarAhmedRazon/
 */
 function make_postcards($attr, $content = null){
+    wp_enqueue_style( 'slider-style', plugin_dir_url( __FILE__ ).'assets/slick/slick.css');
+    wp_enqueue_style( 'slider-base', plugin_dir_url( __FILE__ ).'assets/base.css');
+    wp_enqueue_script('slider-lib', plugin_dir_url( __FILE__ ).'assets/slick/slick.js', array(), null, true);
+    wp_enqueue_script('slider-app', plugin_dir_url( __FILE__ ).'assets/app.js', array(), null, true);
  
     global $post;
  
@@ -31,31 +35,72 @@ function make_postcards($attr, $content = null){
      
     $recent_posts = get_posts($args);
  
-    $post_card = '<div class="post_card_container">';
+    echo '<div class="post_card_container">';
  
     foreach ($recent_posts as $post) :
          
         setup_postdata($post);
-        $post_card .= '<div class="postcard">';
-        $post_card .= '<div class="postcard_body">'.the_content();
-        $post_card .= '</div>';
-        $post_card .= '</div>'; 
+            $content = apply_filters( 'the_content', get_the_content() );
+        echo '<div class="postcard">';
+        echo '<div class="sliderContainer">';
+        echo '<div class="slider" id="slide_'.$post->ID.'" data-slick=\'{"asNavFor": "#slideNav_'.$post->ID.'"}\'>';
+                if(has_post_thumbnail( )){
+                    echo '<img src="'.get_the_post_thumbnail_url().'">';
+                }
+                if( class_exists('Dynamic_Featured_Image') ):
+                    global $dynamic_featured_image;
+                    $featured_images = $dynamic_featured_image->get_featured_images( $post->ID );
+
+                    if ( $featured_images ):
+                        foreach( $featured_images as $images ):
+                            echo '<img src="'.$images['full'].'" alt="">';                            
+                        endforeach; 
+                    endif;
+                endif;
+                
+        echo '</div>'; // Main Slide End
+        echo '<div class="prev btn"></div><div class="next btn"></div>';
+        echo '</div>'; // Slide container
+        echo '<div class="postcard_body">'.$content;
+        echo '<div class="sliderNavcontainer">';
+        echo '<div class="slider-nav" id="slideNav_'.$post->ID.'" data-slick=\'{"asNavFor": "#slide_'.$post->ID.'"}\'>';
+                if(has_post_thumbnail( )){
+                    echo '<img src="'.get_the_post_thumbnail_url($post->ID,'thumbnail').'">';
+                }
+                if( class_exists('Dynamic_Featured_Image') ):
+                    global $dynamic_featured_image;
+                    $featured_images = $dynamic_featured_image->get_featured_images( $post->ID );
+
+                    if ( $featured_images ):
+                        foreach( $featured_images as $images ):
+                            echo '<img src="'.$images['thumb'].'" alt="">';                            
+                        endforeach; 
+                    endif;
+                endif;
+        echo '</div>'; // Nav Slider End
+        echo '<div class="prev btn"></div><div class="next btn"></div>';
+        echo '</div>'; // Nav Slider container End
+        echo '</div>'; // Body Content end
+        echo '</div>'; // PostCard End
+                         
 
     endforeach;    
      
     wp_reset_postdata();
  
-    $post_card .= '</div>';
-     
-    return $post_card;
+    echo '</div><style>.btn:before{background-image:url("'.plugin_dir_url(__File__ ).'assets/arrow.svg");}</style>';
  
 }
-
-add_shortcode( 'postcards', 'make_postcards' );
-
+add_shortcode( 'postcards', 'make_postcards' ); 
 
 
 
+
+function activate_postcard() {
+    theTitleShortCode();
+} 
+
+register_activation_hook( __FILE__, 'activate_postcard' );
 
 function theTitleShortCode(){
     $title = wp_kses_post(get_the_title());
