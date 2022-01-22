c40665f32b4a4c5c0c45e033800fe5d3feef9c13
grimroy <grim.roy@gmail.com>
2022-01-22 17:06:39 +0600

    Slider Responsive Update

--- assets/app.js
+++ assets/app.js
@@ -50,6 +50,22 @@ jQuery.noConflict();
       centerMode: true,
       focusOnSelect: true,
       lazyLoad: "ondemand",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     });
   });
 })(jQuery);
